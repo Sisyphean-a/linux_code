26add9734d32f0b16ae717c0d441a066c0b779f1
Sisyphean-a <362108178@qq.com>
2022-01-08 21:02:19 +0800

    进行了时间整理

---
+++ .history/1_8_pandas_20220108205850.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# pd.DataFrame()生成一个最小的数据集
+df = pd.DataFrame()
+
+# 可以直接输出查看数据集
+print(df)
+
+# to_excel()表示把数据集转换为excel格式，需要提供文件的路径
+df.to_excel("1_8_pandas.xlsx")
+
+print("Done!")
